d3927198b9d71a09ab5d15a7cc6c2fab1c8728de
uzofficial786-svg <uzofficial786@gmail.com>
2026-06-19 18:56:34 +0530

    Add files via upload

---
+++ Root.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Composition } from "remotion";
+import { Scene01 } from "./Scene01";
+
+export const RemotionRoot: React.FC = () => {
+  return (
+    <>
+      <Composition
+        id="Scene01"
+        component={Scene01}
+        durationInFrames={150}
+        fps={30}
+        width={1920}
+        height={1080}
+        defaultProps={{ title: "Hello, Remotion!" }}
+      />
+    </>
+  );
+};
